18743bc145f47977ec40e76c950c9e7da18df618
axiomcura <erikishere3@gmail.com>
2024-12-20 11:16:42 -0700

    updated aggregate function

--- 3.preprocessing_features/scripts/3.aggregate_single_cells.py
+++ 3.preprocessing_features/scripts/3.aggregate_single_cells.py
@@ -8,9 +8,9 @@
 
 
 import pathlib
-
 import pandas as pd
 from pycytominer import aggregate
+
 
 # In[2]:
 
@@ -21,15 +21,11 @@ agg_tag = "aggregated"
 
 # setting up paths
 data_dir = pathlib.Path("./data").resolve(strict=True)
-sc_data_dir = pathlib.Path(
-    "../3.preprocessing_features/data/single_cell_profiles/"
-).resolve(strict=True)
+sc_data_dir = pathlib.Path("../3.preprocessing_features/data/single_cell_profiles/").resolve(strict=True)
 metadata_dir = pathlib.Path("../metadata/updated_platemaps").resolve(strict=True)
 
 # setting metadata paths
-updated_barcode_path = (metadata_dir / "updated_barcode_platemap.csv").resolve(
-    strict=True
-)
+updated_barcode_path = (metadata_dir / "updated_barcode_platemap.csv").resolve(strict=True)
 all_profiles_paths = list(sc_data_dir.glob("*sc_feature_selected.parquet"))
 
 # output files paths
@@ -37,23 +33,43 @@ aggregated_dir_path = (data_dir / "aggregated_profiles").resolve()
 aggregated_dir_path.mkdir(exist_ok=True)
 
 
-# Next, we use the aggregation functionality provided by pycytominer to consolidate single-cell profiles into compound-level summaries for each plate. This step groups the data by a specified metadata column (e.g., Metadata_treatment) and computes aggregate statistics by using the median.
+# In[ ]:
 
-# In[3]:
+
+
+
+
+# Next, we use the aggregation functionality provided by pycytominer to consolidate single-cell profiles into well-level summaries for each plate. This step groups the data by a specified metadata column and computes aggregate statistics by using the median.
+
+# In[42]:
 
 
 # Iterate over all profile file paths to process and aggregate data
 for plate_path in all_profiles_paths:
+
     # Extract the plate name from the file stem
     plate_name = plate_path.stem.split("_", 1)[0]
 
     # Load the single-cell profile data from the current Parquet file into a DataFrame
     profile_df = pd.read_parquet(plate_path)
+    
+    # Create the new column "Metadata_Well" by concatenating 'Metadata_WellRow' and 'Metadata_WellCol'
+    profile_df["Metadata_Well"] = (
+        profile_df["Metadata_WellRow"].astype(str) + profile_df["Metadata_WellCol"].astype(str)
+    )
+
+    # Move the new column to the first position
+    profile_df = profile_df[["Metadata_Well"] + [col for col in profile_df.columns if col != "Metadata_Well"]]
 
     # Apply the aggregation function using pycytominer
     # This step groups the single-cell data by the 'Metadata_treatment' column
     # and computes aggregate statistics to summarize profiles at the compound level
-    agg_df = aggregate(profile_df, strata=["Metadata_treatment"])
-
     # Save the aggregated data to the output directory as a Parquet file
-    agg_df.to_parquet(aggregated_dir_path / f"{plate_name}_{agg_tag}.parquet")
+    agg_df = aggregate(
+        profile_df,
+        strata=["Metadata_WellRow", "Metadata_WellCol"],
+        output_type="parquet",
+        output_file=(aggregated_dir_path / f"{plate_name}_{agg_tag}.parquet").resolve(),
+    )
+
+
