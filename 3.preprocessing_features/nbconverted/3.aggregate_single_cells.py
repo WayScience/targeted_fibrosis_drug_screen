39305a1e7d34d7c313852690b222e19f5073365e
Erik Serrano <erikishere3@gmail.com>
2026-04-06 13:54:06 -0600

    added spherizing

--- 3.preprocessing_features/nbconverted/3.aggregate_single_cells.py
+++ 3.preprocessing_features/nbconverted/3.aggregate_single_cells.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-# coding: utf-8
 
 # ## Aggregating feature-selected single cells
-# 
-# In this notebook, we process single-cell feature-selected profiles to generate compound-level aggregated profiles for each plate using the pycytominer. 
-# The single-cell profiles are grouped by treatment (Metadata_treatment) and are saved as Parquet files in the aggregated_profiles directory. 
+#
+# In this notebook, we process single-cell feature-selected profiles to generate compound-level aggregated profiles for each plate using the pycytominer.
+# The single-cell profiles are grouped by treatment (Metadata_treatment) and are saved as Parquet files in the aggregated_profiles directory.
 # These aggregated profiles provide concise and interpretable data for downstream analysis at the compound level.
 
 # ## Import libraries
 
-# In[1]:
+# In[ ]:
 
 
+import os
 import pathlib
 import pprint
 import random
+
 import pandas as pd
-import os
-
-from pycytominer import aggregate, annotate
-
+from pycytominer import aggregate, annotate, normalize
 
 # ## Set paths and variables
 
-# In[2]:
+# In[ ]:
 
 
 # get the batch to process from environment variable
@@ -45,12 +43,13 @@ else:
     batch_dirs = [p for p in base_dir.glob("batch_*") if p.is_dir()]
 
 
-# In[3]:
+# In[ ]:
 
 
 # parameters
 sc_fs_tag = "sc_feature_selected"
 agg_tag = "aggregated_post_fs"
+spherized_tag = "spherized_post_fs"
 
 # batch to process
 batch_dir = pathlib.Path(f"./data/{batch_to_process}").resolve(strict=True)
@@ -93,7 +92,7 @@ for layout_dir in layout_dirs:
     all_profiles_paths.extend(parquet_files)
 
 
-# In[4]:
+# In[ ]:
 
 
 # Load the barcode_platemap file
@@ -133,6 +132,9 @@ for layout_dir in batch_dir.glob("platemap_*"):
             "output_dir": batch_dir
             / layout_dir.name
             / "aggregated_profiles",  # keeps each plate in the correct layout folder
+            "spherized_output_dir": batch_dir
+            / layout_dir.name
+            / "bulk_spherized_profiles",  # keeps each plate in the correct layout folder
         }
 
 # Confirm we have all plates
@@ -142,7 +144,7 @@ pprint.pprint(plate_info_dictionary, indent=4)
 
 # Next, we use the aggregation functionality provided by pycytominer to consolidate single-cell profiles into well-level summaries for each plate. This step groups the data by a specified metadata column and computes aggregate statistics by using the median.
 
-# In[5]:
+# In[ ]:
 
 
 # Iterate over all profile file paths to process and aggregate data
@@ -173,20 +175,54 @@ for plate, info in plate_info_dictionary.items():
     aggregated_dir_path.mkdir(parents=True, exist_ok=True)
 
     # Perform annotation to make sure that all metadata is added back
-    annotate(
+    annotated_df = annotate(
         profiles=agg_df,
         platemap=platemap_df,
         join_on=["Metadata_well_position", "Metadata_Well"],
+    )
+    # Save the annotated, aggregated profiles to a new Parquet file in the output directory
+    annotated_df.to_parquet((aggregated_dir_path / f"{plate}_{agg_tag}.parquet").resolve())
+
+
+    # spherize the aggregated data
+
+    # Set up output directory
+    spherized_dir_path = info["spherized_output_dir"]
+    spherized_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # select metadata and feature columns for spherization
+    feature_cols = [col for col in annotated_df.columns if not col.startswith("Metadata_")]
+    metadata_cols = [col for col in annotated_df.columns if col.startswith("Metadata_")]
+
+    # remove zero-variance features from the reference population
+    # this will cause the spherization to fail if there are any features that have zero variance (or low)
+    # in the reference population
+    ref_query = "Metadata_treatment == 'DMSO' and Metadata_cell_type == 'failing'"
+    reference_df = annotated_df.query(ref_query)[feature_cols]
+    zero_var = reference_df.columns[reference_df.var() == 0].tolist()
+    cleaned_feature_cols = [f for f in feature_cols if f not in zero_var]
+    print(f"Removed {len(zero_var)} zero-variance features from reference population: {zero_var}")
+
+    normalize(
+        profiles=annotated_df,
+        meta_features=metadata_cols,
+        features=feature_cols,
+        method="spherize",
+        samples="Metadata_treatment == 'DMSO' and Metadata_cell_type == 'failing'",
+        spherize_method = "ZCA-cor",
+        spherize_center = True,
+        spherize_epsilon = 1e-5,
         output_type="parquet",
-        output_file=(aggregated_dir_path / f"{plate}_{agg_tag}.parquet").resolve(),
+        output_file=(spherized_dir_path / f"{plate}_{spherized_tag}.parquet").resolve(),
     )
 
 
-# In[6]:
+# In[ ]:
 
 
 # Get a list of Parquet files in the directory
 parquet_files = list(aggregated_dir_path.glob("*.parquet"))
+spherized_files = list(spherized_dir_path.glob("*.parquet"))
 
 # Check if there are any files in the directory
 if parquet_files:
@@ -205,4 +241,3 @@ print(
     "Metadata columns:", [col for col in test_df.columns if col.startswith("Metadata_")]
 )
 test_df.head(2)
-
