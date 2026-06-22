a61ea4d637ec575876cb6f844043a864d93f6e51
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2026-06-22 08:59:59 -0600

    remove bulk and add themes file for r plots

--- 3b.exploratory_analysis/nbconverted/1.visualize_UMAP.r
+++ 3b.exploratory_analysis/nbconverted/1.visualize_UMAP.r
@@ -2,6 +2,7 @@ suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(arrow))
 suppressPackageStartupMessages(library(ggExtra))
+source("./plot_themes.r")
 
 # Set up output directory for UMAP figures
 output_fig_dir <- file.path("figures", "UMAPs")
@@ -226,20 +227,7 @@ for (plate in names(umap_cp_df)) {
         + theme_bw()
 
         + scale_color_manual(
-            values = c(
-                "healthy + DMSO" = "#004400",
-                "failing + DMSO" = "#a0004b",
-                "UCD-0001921" = "#1b9e77",
-                "UCD-0001812" = "#d95f02",
-                "UCD-0159268" = "#7570b3",
-                "UCD-0159406" = "#e7298a",
-                "UCD-0000841" = "#66a61e",
-                "UCD-0159442" = "#e6ab02",
-                "UCD-0001419" = "#a6761d",
-                "UCD-0159486" = "#984ea3",
-                "UCD-0159487" = "#e41a1c",
-                "Other" = "grey75"
-            ),
+            values = highlight_colors,
             breaks = present_highlights,
             name = "Hit compound(s)"
         )
